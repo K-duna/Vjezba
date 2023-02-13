b1ba3724f28a647d485260a2f5d9767afd88d70a
Kristina <dunaikristina@gmail.com>
2023-02-13 18:53:34 +0100

    Show daTe in HTML

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello Word");
+const dateElement = document.querySelector("#date");
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString();
+
+// function displayTime () {
+//     const date = new Date();
+//     const hours = date.getHours();
+//     const minutes = date.getMinutes();
+//     const seconds = date.getSeconds();
+    
+//     dateElement.innerHTML = `${today} ${hours}h ${minutes}m ${seconds}s`;
+
+// }
+// displayTime()
